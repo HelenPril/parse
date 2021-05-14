d40e725d98dc4017a564641cf4a2ea1f870c683e
HelenPril <helen.pril37@gmail.com>
2021-05-14 21:27:08 +0300

    выполнила задания к 5 уроку

---
+++ gb_parse/hh_loader.py
@@ -0,0 +1,23 @@
+#предоставляют интерфейс для потокового извлечения данных из респонса
+from urllib.parse import urljoin
+from scrapy.loader import ItemLoader
+from itemloaders.processors import TakeFirst, MapCompose
+
+#TakeFirst - выборка первого ЗНАЧЕНИЯ из списка, встроенная ф-ция
+
+def flat_text(items):
+    return "\n".join(items)
+
+def hh_user_url(user_id):
+    return urljoin("https://hh.ru/", user_id)
+
+
+class HhLoader(ItemLoader):
+    default_item_class = dict  #какой тип айтем будет на выходе
+    url_out = TakeFirst() #out - то, что вернется, будет в результирующем словаре, который мы объявили
+    title_out = TakeFirst()
+    salary_out = flat_text
+    description_out = flat_text
+    author_link_in = MapCompose(hh_user_url)
+    author_link_out = TakeFirst()
+
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from scrapy.crawler import CrawlerProcess
 from scrapy.settings import Settings
 from gb_parse.spiders.autoyoula import AutoyoulaSpider
 from gb_parse.spiders.proba import ProbaSpider
+from gb_parse.spiders.hh import HhSpider
 
 
 
@@ -10,5 +11,6 @@ if __name__ == "__main__":
     crawler_settings.setmodule("gb_parse.settings")
     crawler_process = CrawlerProcess(settings=crawler_settings)
     #crawler_process.crawl(AutoyoulaSpider)
-    crawler_process.crawl(ProbaSpider)
+    #crawler_process.crawl(ProbaSpider)
+    crawler_process.crawl(HhSpider)
     crawler_process.start()
