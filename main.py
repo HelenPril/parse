d54be32acc60d9d1c4a4e36be73c90a7b7e8f68c
HelenPril <helen.pril37@gmail.com>
2021-05-15 11:05:39 +0300

    выполнила задания к 5 уроку

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from scrapy.crawler import CrawlerProcess
 from scrapy.settings import Settings
-from gb_parse.spiders.autoyoula import AutoyoulaSpider
-from gb_parse.spiders.proba import ProbaSpider
-from gb_parse.spiders.hh import HhSpider
+
+from gb_parse.spiders.hh_final import HhSpider
 
 
 
@@ -11,6 +10,5 @@ if __name__ == "__main__":
     crawler_settings.setmodule("gb_parse.settings")
     crawler_process = CrawlerProcess(settings=crawler_settings)
     #crawler_process.crawl(AutoyoulaSpider)
-    #crawler_process.crawl(ProbaSpider)
     crawler_process.crawl(HhSpider)
     crawler_process.start()
